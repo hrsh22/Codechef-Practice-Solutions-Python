d6029db035e02c14dd9bad5eb200c2d53bb4adce
Harsh Gupta <90423812+hrsh22@users.noreply.github.com>
2022-06-08 22:53:22 +0530

    Add files via upload

---
+++ SEMCOURSES.py
@@ -0,0 +1,4 @@
+# cook your dish here
+for _ in range(int(input())):
+    X,Y,Z=map(int,input().split())
+    print(X*Y*Z)
---
+++ SUBSCRIBE.py
@@ -0,0 +1,7 @@
+# cook your dish here
+for _ in range(int(input())):
+    a=int(input())
+    if a>30:
+        print("YES")
+    else:
+        print("NO")
