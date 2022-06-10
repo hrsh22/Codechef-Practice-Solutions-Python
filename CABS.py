00b9ea02cde05e130ce4b33077ed9c44cd20d9b3
Harsh Gupta <90423812+hrsh22@users.noreply.github.com>
2022-06-10 23:36:44 +0530

    Add files via upload

---
+++ CABS.py
@@ -0,0 +1,10 @@
+# cook your dish here
+T=int(input())
+for _ in range(T):
+    X,Y=map(int,input().split())
+    if X<Y:
+        print("FIRST")
+    elif X>Y:
+        print("SECOND")
+    else:
+        print("ANY")
