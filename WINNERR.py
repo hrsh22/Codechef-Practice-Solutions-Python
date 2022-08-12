9889fd6afd3a979b2f42e0a18fac1e7d8a1dc34b
Harsh Gupta <90423812+hrsh22@users.noreply.github.com>
2022-08-13 00:00:09 +0530

    Add files via upload

---
+++ WINNERR.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    pa, pb, qa, qb = map(int, input().split())
+    
+    if max(pa, pb) < max(qa, qb):
+        print('P')
+    elif max(pa, pb) > max(qa, qb):
+        print('Q')
+    else:
+        print("TIE")
