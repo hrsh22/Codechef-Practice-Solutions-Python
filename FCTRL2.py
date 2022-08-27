a33d1fc6493607d4a66bcf0b309779ca95168f20
Harsh Gupta <90423812+hrsh22@users.noreply.github.com>
2022-08-28 03:15:47 +0530

    Add files via upload

---
+++ FCTRL2.py
@@ -0,0 +1,15 @@
+def factorial(n):
+    if n==0:
+        return 1
+    elif n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+n=int(input())
+for i in range(n):
+    num=int(input())
+    print(factorial(num))
+
+
+
+
